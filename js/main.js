2741ac01cc177113920d0e2df137b0135a77a206
koki <koh724@gmail.com>
2021-04-05 07:59:28 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -26,15 +26,42 @@ const form = document.querySelector('form');
 const messages = document.getElementById('messages');
 const auth = firebase.auth();
 
+const login = document.getElementById('login');
+const logout = document.getElementById('logout');
 
-collection.orderBy('created').onSnapshot(snapshot => {
-  snapshot.docChanges().forEach(change => {
-    if (change.type === 'added') {
-      const li = document.createElement('li');
-      li.textContent = change.doc.data().message;
-      messages.appendChild(li);
+
+login.addEventListener('click',() =>{
+    auth.signInAnonymously();
+});
+
+logout.addEventListener('click',()=>{
+    auth.signOut();
+});
+
+auth.onAuthStateChanged(user =>{
+    if(user){
+        collection.orderBy('created').onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+              if (change.type === 'added') {
+                const li = document.createElement('li');
+                li.textContent = change.doc.data().message;
+                messages.appendChild(li);
+              }
+            });
+          });
+          console.log(`Logged in user as: ${user.uid}`);
+          login.classList.add('hidden');
+          [logout,form,messages].forEach(el =>{
+            el.classList.remove('hidden');
+          });
+          message.focus();
+          return;
     }
-  });
+    console.log('Nobody is logged in');
+    login.classList.remove('hidden');
+    [logout,form,messages].forEach(el =>{
+        el.classList.add('hidden');
+    });
 });
 
 form.addEventListener('submit', e => {
@@ -60,4 +87,3 @@ form.addEventListener('submit', e => {
   });
 });
 
-message.focus();
